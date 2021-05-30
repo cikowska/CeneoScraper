d92e950b3d425940ec8663a080f95a7cf16ec58b
cikowska <77065729+cikowska@users.noreply.github.com>
2021-05-30 16:27:06 +0200

    2021-05-30 KC

--- scraper.py
+++ scraper.py
@@ -1,45 +1,65 @@
+import re
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
+import json
 
-response =requests.get("https://www.ceneo.pl/99756635#tag=nph_row_promotion")
+from requests.api import options
+
+def extract_element(dom_tree, selector, attribute=None):
+    try:
+        if  isinstance(attribute,str):
+           return dom_tree.select_one(selector)[attribute].strip()
+        if isinstance(attribute,list):
+            return [p.text.strip() for e in dom_tree(selector)]
+        return dom_tree.select_one(selector).text.strip()
+    except (AttributeError, TypeError):
+        return  None
+    
+
+
 #print(response.status_code)
 #print(response.text)
 
-page_dom = BeautifulSoup(response.text, "html.parser")
+
 
 '''reviews = page_dom.select("div.js_product-review")
 print(type(reviews))
 review = reviews.pop(0)
 print(type(review))'''
-review = page_dom.select_one("div.js_product-review")
-print(type(review))
 
-review_id = review["data-entry-id"]
 
-author = review.select_one("span.user-post__author-name").text.strip()
+all_reviews = []
 
-recommendation = review.select_one("span.user-post__author-recomendation").text.strip()
+service_url = "https://www.ceneo.pl"
+next_page ="/99756635#tag=nph_row_promotion"
+response =requests.get(service_url+next_page)
+while next_page:
+    page_dom = BeautifulSoup(response.text, "html.parser")
+    reviews = page_dom.select("div.js_product-review")
 
-#recommendation = review.select_one("span.user-post__author-recomendation > em").string.strip()
-stars = review.select_one("span.user-post__score-count").text.strip()
-
-content = review.select_one("div.user-post__text").text.strip()
-
-pros = review.select("div.review-feature__title--positives ~ div.review-feature__item")
-#print(pros.pop(0).text.strip())
-
-cons = review.select("div.review-feature__title--negatives ~ div.review-feature__item")
-
-useful = review.select_one("button.vote-yes > span").text.strip()
-
-useless = review.select_one("button.vote-no > span").text.strip()
-
-purchase = review.select_one("div.review-pz").text.strip()
-
-review_date = review.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
-
-purchase_date = review.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
-
-print(purchase_date)
-exit()
-
+    for review in reviews:
+        single_review = {
+            "review_id": review["data-entry-id"],
+            "author": extract_element(review,"span.user-post__author-name"),
+            "recommendation":  extract_element(review,"span.user-post__author-recomendation"),
+            #recommendation:  review.select_one("span.user-post__author-recomendation > em").string.strip()
+            "stars":  extract_element(review,"span.user-post__score-count"),
+            "content":  extract_element(review,"div.user-post__text"),
+            #pros:  review.select("div.review-feature__title--positives ~ div.review-feature__item")
+            "pros":  extract_element(review,"div.review-feature__title--positives ~ div.review-feature__item",[]),
+            #print(pros.pop(0).text.strip())
+            "cons":  extract_element(review,"div.review-feature__title--negatives ~ div.review-feature__item",[]),
+            "useful":  extract_element(review,"button.vote-yes > span"),
+            "useless":  extract_element(review,"button.vote-no > span"),
+            "purchase":  extract_element(review, "div.review-pz"),
+            "review_date":  extract_element(review, "span.user-post__published > time:nth-child(1)", "datetime"),
+            "purchase_date":  extract_element(review, "span.user-post__published > time:nth-child(2)", "datetime"),
+        }
+        all_reviews.append(single_review)
+        next_page = extract_element(page_dom, "a.pagination__next", "href")
+with open("options/abc.json, "w", encoding="UTF-8") as js:
+    json.dumps(all_reviews, ensure_ascii=False, indent=4))
+    
+    '''pros_list: [] można to zrobić krócej za pomocą list comprehension
+    for p in pros:
+        pros_list.append(p.text.strip())'''
